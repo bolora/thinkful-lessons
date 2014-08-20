6082f1fc936ea5030c08167894ab6ddc436f3b90
Bo Lora <bo_lora@dell.com>
2014-08-20 07:05:47 -0500

    fixed ajax loader issue and did a new build

--- dist/lessons/unit-2/open-weather-app/services/store-spec.js
+++ dist/lessons/unit-2/open-weather-app/services/store-spec.js
@@ -1,8 +1,8 @@
-describe('store', function() {
+describe('owaStore', function() {
 
   beforeEach(module('openWeatherApp'));
 
-  it('should ...', inject(function(store) {
+  it('should ...', inject(function(owaStore) {
 
 	//expect(store.doSomething()).toEqual('something');
 
--- lessons/unit-2/open-weather-app/open-weather-app.js
+++ lessons/unit-2/open-weather-app/open-weather-app.js
@@ -4,7 +4,7 @@ angular.module('openWeatherApp').config(function($stateProvider) {
 
     $stateProvider.state('owaHome', {
         url: '/lessons/unit-2/open-weather-app',
-        templateUrl: '/lessons/unit-2/open-weather-app/partials/home/home.html'
+        templateUrl: 'lessons/unit-2/open-weather-app/partials/home/home.html'
     });
     $stateProvider.state('locationSearch', {
         url: '/lessons/unit-2/open-weather-app/:locationSearch',
